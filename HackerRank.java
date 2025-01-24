1dc3e20bf6f03188c73184f2021b08096da5ccac
tusharkhed04 <tusharkhedkar04@gmail.com>
2025-01-24 08:58:27 +0530

    hourglass

--- HackerRank.java
+++ HackerRank.java
@@ -217,3 +217,29 @@ public class Solution {
 }
 
   */
+
+  /*--------------------------------------------------Hourglass in array--------------------------------------------------
+    public static void main(String[] args) {
+        int maxSum = Integer.MIN_VALUE;
+        Scanner sc = new Scanner(System.in);
+        int[][] a = {{1,1,1,0,0,0,0},{0,1,0,0,0,0,0},{1,1,1,0,0,0,0},{0,9,2,-4,-4,0},{0,0,0,-2,0,0,0},{0,0,-1,-2,-4,0}};
+        for(int i=0;i<6;i++){
+            for(int j=0;j<6;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+
+        for(int i =0;i<4;i++){
+            for(int j=0;j<4;j++){
+                int sum = a[i][j] +  a[i][j+1] + a[i][j+2] +  a[i+1][j+1] +  a[i+2][j] +  a[i+2][j+1] +  a[i+2][j+2];
+                if(maxSum <sum){
+                    maxSum = sum;
+                }
+            }
+        }
+        System.out.println(maxSum);
+    }
+   * 
+   */
