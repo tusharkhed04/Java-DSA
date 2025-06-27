98d20e12b8b899ba3d069b2993fefa5de471aa69
tusharkhed04 <tusharkhedkar04@gmail.com>
2025-06-27 07:36:42 +0530

    update

--- Hashing_map.java
+++ Hashing_map.java
@@ -13,39 +13,39 @@ public class Hashing_map {
         map.put("China", 180);
         map.put("UK", 70);
 
-        //System.out.println(map);
+        System.out.println(map);
 
         // Updation
 
-        // map.put("India", 140);
-        // System.out.println(map);
+        map.put("India", 140);
+        System.out.println(map);
 
         // Search
 
-        // if(map.containsKey("UK")) {
-        //     System.out.println("Key is present in the map");
-        // } else {
-        //     System.out.println("Key is not present in the map");
-        // }
+        if(map.containsKey("UK")) {
+            System.out.println("Key is present in the map");
+        } else {
+            System.out.println("Key is not present in the map");
+        }
 
         // Get element from map
 
-        // System.out.println(map.get("India"));
-        // System.out.println(map.get("USA"));
+        System.out.println(map.get("India"));
+        System.out.println(map.get("USA"));
 
         // Iteration
 
-        // for(Map.Entry<String, Integer> e: map.entrySet()) {
-        //     System.out.println(e.getKey()+" : "+e.getValue()+",");
-        // }
-        // System.out.println();
+        for(Map.Entry<String, Integer> e: map.entrySet()) {
+            System.out.println(e.getKey()+" : "+e.getValue()+",");
+        }
+        System.out.println();
 
         // iteration using set
 
-        // Set<String> keys = map.keySet();
-        // for(String key : keys) {
-        //     System.out.println(key+" => "+map.get(key));
-        // }
+        Set<String> keys = map.keySet();
+        for(String key : keys) {
+            System.out.println(key+" => "+map.get(key));
+        }
 
 
         // Remove
